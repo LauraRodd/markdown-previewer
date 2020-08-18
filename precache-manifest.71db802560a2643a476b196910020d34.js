13e0add2e1b01fba6a8951a7cafeebe6f4b5059f
Laura Rodd <laura_rodd@hotmail.com>
2020-08-18 14:46:36 +0200

    Updates

---
+++ precache-manifest.71db802560a2643a476b196910020d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2403eefa7c00ccad9b033f27e0ca0f06",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "a982aa7e328571754414",
+    "url": "/markdown-previewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a982aa7e328571754414",
+    "url": "/markdown-previewer/static/js/2.28dbede0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/markdown-previewer/static/js/2.28dbede0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd59ef92ea42191bf3b",
+    "url": "/markdown-previewer/static/js/main.fb37bf4f.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
